8e4d19ce98003e73802e8d1936c297b2889a42d2
udit2720001 <udit2720001@gmail.com>
2025-11-08 22:22:57 +0530

    hello world

---
+++ qemu_hello/build/zephyr/zephyr.dts.d
@@ -0,0 +1,5 @@
+empty_file.o: /home/udit27/zephyrproject/zephyr/misc/empty_file.c \
+ /home/udit27/zephyrproject/zephyr/boards/qemu/cortex_m3/qemu_cortex_m3.dts \
+ /home/udit27/zephyrproject/zephyr/dts/arm/ti/lm3s6965.dtsi \
+ /home/udit27/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/udit27/zephyrproject/zephyr/dts/common/skeleton.dtsi
